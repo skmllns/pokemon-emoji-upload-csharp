f18eba058dc47b4ff17248dc49f03a7a59cf905a
Sarah Mullins <skmllns@gmail.com>
2016-11-21 17:36:33 -0500

    initial commit

---
+++ BatchPokemonEmoji/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace BatchPokemonEmoji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadImages();
+            ResizeImages();
+            UploadImages();
+        }
+
+        static void DownloadImages()
+        {
+     
+        }
+
+        static void ResizeImages()
+        {
+            
+        }
+
+        static void UploadImages()
+        {
+
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DownloadImagesTest()
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser
+            {
+                AllowAutoRedirect = true,
+                AllowMetaRedirect = true
+            };
+            int x = 0;
+            WebPage PageResult = Browser.NavigateToPage(new Uri("http://www.pokemon.com/us/pokedex/"));
+            //HtmlNode ul = PageResult.Html.CssSelect("ul.results").First();
+            //IEnumerable<HtmlNode> li = ul.Descendants();
+            HtmlNode section = PageResult.Html.CssSelect("section.pokedex-results").First();
+            foreach (var li in section.SelectNodes("ul"))
+            {
+                x++;
+            }
+        }
+    }
+}
