e83c72c206811835d0ec555b8d2cfbf33439e25a
Sarah Mullins <skmllns@gmail.com>
2016-11-22 17:18:19 -0500

    DownloadImages function added, ResizeImages just needs saving issues sorted out

--- BatchPokemonEmoji/Program.cs
+++ BatchPokemonEmoji/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ScrapySharp.Extensions;
+using HtmlAgilityPack;
 using ScrapySharp.Network;
+using System.Net;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace BatchPokemonEmoji
 {
@@ -12,19 +13,59 @@ namespace BatchPokemonEmoji
     {
         static void Main(string[] args)
         {
-            DownloadImages();
-            ResizeImages();
+            //string filePath = DownloadImages();
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo fileInfo = new FileInfo(baseDirectory);
+            DirectoryInfo parentDir = fileInfo.Directory.Parent.Parent;
+            string filePath = parentDir.FullName + @"\Downloads\";
+
+            ResizeImages(filePath);
             UploadImages();
         }
 
-        static void DownloadImages()
+        static string DownloadImages()
         {
-     
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            WebClient webClient = new WebClient();
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo fileInfo = new FileInfo(baseDirectory);
+            DirectoryInfo parentDir = fileInfo.Directory.Parent.Parent;
+            string parentDirName = parentDir.FullName;
+
+            Uri uri = new Uri("http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon");
+
+            WebPage PageResult = Browser.NavigateToPage(uri);
+
+            HtmlNodeCollection pokeNodes = PageResult.Html.SelectNodes("//div[@id='gallery-0']//div[@class='wikia-gallery-item']");
+
+            //venonat, venomoth, nidoran m/f, farfetch'd
+            foreach (var poke in pokeNodes)
+            {
+                HtmlNode nameNode = poke.SelectNodes("div[@class='thumb']/div").First();
+                string pokeName = nameNode.Id;
+                HtmlNode imageNode = nameNode.SelectNodes("a/img").First();
+                HtmlAttribute attribute = imageNode.Attributes.First(attr => attr.Name == "data-src");
+
+                string dataUrl = attribute.Value;
+                string fileName = parentDirName + @"\Downloads\" + pokeName + @".png";
+                webClient.DownloadFile(dataUrl, fileName);
+            }
+
+            return parentDirName + @"\Downloads\";
         }
 
-        static void ResizeImages()
+        static void ResizeImages(string parentPath)
         {
-            
+            //foreach (var file in Directory.EnumerateFiles(parentPath))
+            //{
+            //    using (var img = Image.FromFile(file))
+            //    {
+            //        Image thumbnail = img.GetThumbnailImage(128, 128, () => false, IntPtr.Zero);
+            //        File.Delete(file);
+            //        thumbnail.Save(file, ImageFormat.Png);
+            //    }
+            //}
         }
 
         static void UploadImages()
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -5,6 +5,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrapySharp.Extensions;
 using ScrapySharp.Network;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace UnitTests
 {
@@ -14,20 +20,82 @@ namespace UnitTests
         [TestMethod]
         public void DownloadImagesTest()
         {
-            ScrapingBrowser Browser = new ScrapingBrowser
+
+            ScrapingBrowser Browser = new ScrapingBrowser();
+
+            Uri uri = new Uri("http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon");
+
+            WebPage PageResult = Browser.NavigateToPage(uri);
+
+            HtmlNodeCollection pokeNodes = PageResult.Html.SelectNodes("//div[@id='gallery-0']//div[@class='wikia-gallery-item']");
+
+            //venonat, venomoth, nidoran m/f, farfetch'd
+            foreach (var poke in pokeNodes)
             {
-                AllowAutoRedirect = true,
-                AllowMetaRedirect = true
-            };
-            int x = 0;
-            WebPage PageResult = Browser.NavigateToPage(new Uri("http://www.pokemon.com/us/pokedex/"));
-            //HtmlNode ul = PageResult.Html.CssSelect("ul.results").First();
-            //IEnumerable<HtmlNode> li = ul.Descendants();
-            HtmlNode section = PageResult.Html.CssSelect("section.pokedex-results").First();
-            foreach (var li in section.SelectNodes("ul"))
-            {
-                x++;
+                HtmlNode nameNode = poke.SelectNodes("div[@class='thumb']/div").First();
+                Console.WriteLine(nameNode.Id);
+
             }
+
+
+        }
+
+
+        [TestMethod]
+        public void DownloadFileTest()
+        {
+
+            WebClient webClient = new WebClient();
+            string url =
+                "http://vignette3.wikia.nocookie.net/pokemon/images/d/d3/049Venomoth.png/revision/latest/scale-to-width-down/200?cb=20140328194046";
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo fileInfo = new FileInfo(baseDirectory);
+            DirectoryInfo parentDir = fileInfo.Directory.Parent;
+            string parentDirName = parentDir.FullName;
+            string fileName = parentDirName + @"\Downloads\Venomoth.png";
+            webClient.DownloadFile(url, fileName);
+
+        }
+
+
+        [TestMethod]
+        public void GetHtmlTest()
+        {
+
+            using (WebBrowser browser = new WebBrowser())
+            {
+                
+                Uri uri = new Uri("http://www.pokemon.com/us/pokedex/");
+                browser.Navigate(uri);
+                browser.DocumentCompleted += browser_DocumentCompleted;
+
+
+            }
+        }
+
+        [TestMethod]
+        public void ResizeImagesTest()
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo fileInfo = new FileInfo(baseDirectory);
+            DirectoryInfo parentDir = fileInfo.Directory.Parent;
+            string filePath = parentDir.FullName + @"\Downloads\";
+
+            foreach (var file in Directory.EnumerateFiles(filePath))
+            {
+                using (var img = Image.FromFile(file))
+                {
+                    Image thumbnail = img.GetThumbnailImage(128, 128, () => false, IntPtr.Zero);
+                    string saveFilePath = filePath + @"y.png";
+                    thumbnail.Save(saveFilePath, ImageFormat.Png);
+                }
+            }
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Console.WriteLine("document finished loading");
+     
         }
     }
 }
